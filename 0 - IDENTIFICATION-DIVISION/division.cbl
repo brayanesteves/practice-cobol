8ec0ae1240a81a9c5a59c619808da142547aa6da
Brayan Esteves <brayan.esteves93@gmail.com>
2023-08-16 12:35:27 -0400

    IDENTIFICATION DIVISION [16/08/2023]

---
+++ 0 - IDENTIFICATION-DIVISION/division.cbl
@@ -0,0 +1,15 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Division.
+       AUTHOR. Develop_easy
+       INSTALLATION.
+       DATE-WRITTEN.16/08/2023
+       DATE-COMPILED.
+       REMARKS.Program created for division of COBOL.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+           DISPLAY "Hello world"
+           STOP RUN.
+       END PROGRAM Division.
