a2d971b11584682d3f453fa4b5cbc5674b645898
Brayan Esteves <brayan.esteves93@gmail.com>
2023-08-19 16:16:39 -0400

    DATA DIVISION [19/08/2023]

---
+++ 2 - DATA-DIVISION/data_division.cbl
@@ -0,0 +1,14 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. DataDivision.
+       ENVIRONMENT DIVISION.
+
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       linkage section.
+       
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+           STOP RUN.
+       END PROGRAM DataDivision.
+       
