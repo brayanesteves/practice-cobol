9fc0f6584de906dbb3791c78f79ea1ad390f6872
Brayan Esteves <brayan.esteves93@gmail.com>
2023-09-02 22:43:48 -0400

    COMPOSITE VARIABLE (Is Array) With Number [02/09/2023]

---
+++ 15 - COMPOSITE VARIABLES/CompositeVariables_IsArray_Num.cbl
@@ -0,0 +1,19 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. CompositeVariablesIsArrayNum.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+
+       01  SINGLE-VARIABLE PIC 9.
+       01  COMPOSITE-VARIABLE-ISARRAY.
+           02 NUM1 PIC 9 VALUE 2.
+           02 NUM2 PIC 9 VALUE 4.
+           02 NUM3 PIC 9 VALUE 4.
+           02 NUM4 PIC 9 VALUE 9.
+
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+           DISPLAY NUM3.
+           DISPLAY COMPOSITE-VARIABLE-ISARRAY.
+            STOP RUN.
+       END PROGRAM CompositeVariablesIsArrayNum.
