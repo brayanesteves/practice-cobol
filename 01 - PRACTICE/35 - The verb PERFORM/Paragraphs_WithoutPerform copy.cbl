00819f0ca3aece6679dfd78c44885938409303e7
Brayan Esteves <brayan.esteves93@gmail.com>
2024-09-19 10:22:55 -0400

    The verb PERFORM [19/09/2024]

---
+++ 01 - PRACTICE/35 - The verb PERFORM/Paragraphs_WithoutPerform copy.cbl
@@ -0,0 +1,22 @@
+      IDENTIFICATION DIVISION.
+      PROGRAM-ID. Paragraphs_WithoutPerform.
+
+      ENVIRONMENT DIVISION.
+      CONFIGURATION SECTION.
+      SPECIAL-NAMES.
+      DATA DIVISION.
+      FILE SECTION.
+      WORKING-STORAGE SECTION.      
+
+      PROCEDURE DIVISION.
+      Paragraph1.
+           DISPLAY "You are in paragraph 1.".
+
+      Paragraph2.
+           DISPLAY "You are in paragraph 2.".
+           DISPLAY "Here is the trip.".
+           STOP RUN.
+      Paragraph3.
+           DISPLAY "You are in paragraph 3.".
+
+      END PROGRAM Paragraphs_WithoutPerform.
